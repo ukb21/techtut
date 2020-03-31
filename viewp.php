13c8e377ed152cf73904cf62d744f5b7c8071957
ukb21 <ukb21@vt.edu>
2020-03-31 16:46:35 -0400

    fsdf

--- viewp.php
+++ viewp.php
@@ -11,7 +11,7 @@ $Request_Search= "";
      <meta charset="utf-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1">
-     <title>View My Sheet Music</title>
+     <title>View</title>
 
      <!-- bootstrap -->
      <link href="css/bootstrap.min.css" rel="stylesheet" />
